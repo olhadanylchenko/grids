4841acb572eb1e85262a549289a9e9568ada184d
Olha Danylchenko <olhadanylchenko@Air-Olha.fritz.box>
2019-10-06 22:26:11 +0200

    make CLEAR transparent: only appearing in front of the black circle.

--- grid2/grid2.js
+++ grid2/grid2.js
@@ -1,4 +1,12 @@
-document.addEventListener('scroll', (e) => {
-    console.log(e)
-}
-)
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({ triggerElement: "#trigger1" })
+    // trigger animation by adding a css class
+    .setClassToggle("#circle", "zap")
+    .triggerHook(0.4) // add indicators (requires plugin)
+    .addTo(controller);
+
+// document.addEventListener('scroll', (e) => {
+//     console.log(e)
+// }
+// )
